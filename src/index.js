c1d73f8e46e520626e26353eaa2807b82745358a
Alexandra Ligacheva <alexandra-gall@mail.ru>
2020-03-20 00:11:28 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = '' ;
+    let i= 0;
+
+    while (i < bracketsConfig.length) {
+        brackets = bracketsConfig[i].join('');
+
+        if (str.includes(brackets)) {
+            str = str.replace(brackets,'');
+            i = 0;
+        } else {
+            i += 1;
+        }
+    }
+
+    return str.length === 0;
 }
